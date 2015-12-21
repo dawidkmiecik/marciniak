c960c55466dad12f80c1385ac48ae377f0cd4ad3
dawidkmiecik <dawidkmiecik@vp.pl>
2015-12-21 20:26:17 +0100

    d

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Marciniak</title>
+
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link href="css/style.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/custom.css">
+
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="js/button.js"></script>
+		<script src="js/carousel.js"></script>
+		<script src="js/bootstrap-dropdownhover.min.js"></script>
+		<script src="js/transition.js"></script>
+		<script src="js/affix.js"></script>
+		<script src="js/alert.js"></script>
+		<script src="js/button.js"></script>
+		<script src="js/carousel.js"></script>
+		<script src="js/collapse.js"></script>
+		<script src="js/modal.js"></script>
+		<script src="js/popover.js"></script>
+		<script src="js/dropdown.js"></script>
+		<script src="js/tab.js"></script>
+		<script src="js/tooltip.js"></script>
+		<script src="js/transition.js"></script>
+
+
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Brand</a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Start</a></li>
+		        <li><a href="#">Usługi</a></li>
+		        <li><a href="#">O nas</a></li>
+		        <li><a href="#">Certyfikaty</a></li>
+		        <li><a href="#">Kontakt</a></li>
+		        
+		      </ul>
+	
+
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			</ol>
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img class="img-responsive" src="img/home.jpg" alt="...">
+					<div class="carousel-caption">
+						...
+					</div>
+				</div>
+				<div class="item">
+					<img class="img-responsive" src="img/painter2.jpg" alt="...">
+					<div class="carousel-caption">
+						...
+					</div>
+				</div>
+			</div>
+
+			<!-- Controls -->
+			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+
+<!-- <section class="offer">
+
+
+
+		<div class="col-md-6 offerSlider">
+
+			    <!-- Full Page Image Background Carousel Header 
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators 
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for Slides
+        <div class="carousel-inner">
+					<div class="offertCenter">
+					<h2 class="offerTitle">Biura i Sklepy</h2>
+					</div>
+
+
+            <div class="item active">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 1</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the second background image using inline CSS below.
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 2</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the third background image using inline CSS below.
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 3</h2>
+                </div>
+            </div>
+        </div>
+
+      
+	</div>
+    </header>
+	
+		<div class="col-md-6 offerSlider">
+			    <!-- Full Page Image Background Carousel Header 
+    <header id="myCarousel2" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel2" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel2" data-slide-to="1"></li>
+            <li data-target="#myCarousel2" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner">
+        	<h2 class="text-center offerTitle">Biura i Sklepy</h2>
+            <div class="item active">
+                <!-- Set the first background image using inline CSS below. 
+                <div class="fill"><span></span></div>
+                <div class="carousel-caption">
+                    <h2>Caption 1</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the second background image using inline CSS below. 
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 2</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the third background image using inline CSS below.
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 3</h2>
+                </div>
+            </div>
+        </div>
+
+        
+
+    </header>
+</div>
+</section>
+ -->	<footer class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<hgroup>
+					<h3>Dane kontaktowe</h3>
+					<h4>Firma Krzak</h4>
+				</hgroup>
+				<address>
+					<p>ul. Krzak 2</p>
+					<p>66-554 Wawa</p>
+					<p>tel. 4444 444 44</p>
+				</address>
+			</div>
+			<div class="col-md-5">
+				<h3>Mapa</h3>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9914406081784!2d2.292292615536884!3d48.858373608662156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sWie%C5%BCa+Eiffla!5e0!3m2!1spl!2spl!4v1450644230821" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+			<div class="col-md-4">
+				<h3>Szybki kontakt</h3>
+				<div class="contactForm">
+					<form method="post" action="enrollment.php">
+						<input name="name" placeholder="Twoje imię i nazwisko">
+						<input name="email" type="email" placeholder="Twój e-mail">
+						<input name="tel" type="tel" placeholder="Twój numer telefonu">
+						<textarea name="message" placeholder="Napisz tu wiadomość"></textarea>
+						<input id="submit" name="submit" type="submit" value="Wyślij">
+					</form>
+				</div><!-- contactForm -->
+			</div>
+		</div>
+	</footer>
+
+	<!-- Script to Activate the Carousel -->
+
+	</body>
+<html>

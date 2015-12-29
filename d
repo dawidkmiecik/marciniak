c47d375d94d831111777ef280385ae1cbb3976ee
dawidkmiecik <dawidkmiecik@vp.pl>
2015-12-29 22:04:03 +0100

    vol.3

---
+++ d
@@ -0,0 +1,57 @@
+		<section class="offer"> 
+			<div class="container">
+				<div class="row">
+					<h2 class="text-center">Domy i Mieszkania</h2>
+					<div class="col-md-5">
+						<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner" role="listbox">
+								<div class="item active">
+									<img src="img/painter2.jpg" alt="...">
+									<div class="carousel-caption">
+										...
+									</div>
+								</div>
+
+								<div class="item">
+									<img src="img/painter2.jpg" alt="...">
+									<div class="carousel-caption">
+										...
+									</div>
+								</div>
+							</div><!-- carousel-innter -->
+						</div><!-- carousel -->
+					</div><!-- col-md-5 -->
+
+					<div class="col-md-7 offerContent">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus a iure pariatur, mollitia modi omnis voluptas quasi soluta ad officia?</p>
+					</div>
+
+				</div><!-- row -->	
+					
+				<div class="row offerContentMargin">
+					<h2 class="text-center">Sklepy i Biura</h2>
+					<div class="col-md-6 offerContent">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis eum necessitatibus ipsum cupiditate eveniet qui nostrum quisquam ullam consequatur sit.</p>
+					</div>	
+					<div class="col-md-6">
+						<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+						    <div class="carousel-inner" role="listbox">
+							    <div class="item active">
+							      <img src="img/painter2.jpg" alt="...">
+							      <div class="carousel-caption">
+							        	...
+							      </div>
+							    </div>
+
+							    <div class="item">
+							      <img src="img/painter2.jpg" alt="...">
+							      <div class="carousel-caption">
+							        	...
+							      </div>
+							    </div>
+						    </div><!-- carousel-inner -->
+						</div><!-- carousel -->
+					</div><!-- col-md=6 -->
+				</div><!-- row -->
+			</div><!-- container -->
+		</section>
--- index.php
+++ index.php
@@ -31,58 +31,66 @@
 
 	</head>
 	<body>
-		<nav class="navbar navbar-default">
-		  <div class="container-fluid">
-		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		      <a class="navbar-brand" href="#">Brand</a>
-		    </div>
-
-		    <!-- Collect the nav links, forms, and other content for toggling -->
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul class="nav navbar-nav navbar-right">
-		        <li><a href="#">Start</a></li>
-		        <li><a href="#">Usługi</a></li>
-		        <li><a href="#">O nas</a></li>
-		        <li><a href="#">Certyfikaty</a></li>
-		        <li><a href="#">Kontakt</a></li>
-		        
-		      </ul>
 	
+		<section class="top"> 
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Brand</a>
+					</div>
 
-		    </div><!-- /.navbar-collapse -->
-		  </div><!-- /.container-fluid -->
-		</nav>
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-			<!-- Indicators -->
+				<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Start</a></li>
+						<li><a href="#">Usługi</a></li>
+						<li><a href="#">O nas</a></li>
+						<li><a href="#">Kontakt</a></li> 
+					</ul>
+
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</section>
+
+		<div id="carousel-example-generic" class="carousel slide topCarouselBorder" data-ride="carousel">
+			
 			<ol class="carousel-indicators">
 				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
 			</ol>
 
-			<!-- Wrapper for slides -->
-			<div class="carousel-inner" role="listbox">
+			
+			<div class="carousel-inner topCarousel" role="listbox">
 				<div class="item active">
 					<img class="img-responsive" src="img/home.jpg" alt="...">
-					<div class="carousel-caption">
-						...
+					<div class="carousel-caption topCarouselContent">
+						<p>Planujesz remont ?</p>
+						<p>Trafiłeś w 10!</p>
+						<button class="btn">Zobacz</button>
 					</div>
 				</div>
 				<div class="item">
 					<img class="img-responsive" src="img/painter2.jpg" alt="...">
-					<div class="carousel-caption">
-						...
+					<div class="carousel-caption topCarouselContentTwo">
+						<p>Robimy:</p>
+						<li>Adaptacje poddaszy</li>
+						<li>Kompleksowne remoty</li>
+						<li>Malowanie i Tapetowanie</li>
+						<li>Układanie glazury i terakoty</li>
+						<li>Instalacje elektryczne i kanalizacyjne</li>
+
 					</div>
 				</div>
 			</div>
 
-			<!-- Controls -->
+		
 			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
 				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 				<span class="sr-only">Previous</span>
@@ -93,128 +101,100 @@
 			</a>
 		</div>
 
-<!-- <section class="offer">
-
-
-
-		<div class="col-md-6 offerSlider">
-
-			    <!-- Full Page Image Background Carousel Header 
-    <header id="myCarousel" class="carousel slide">
-        <!-- Indicators 
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-
-        <!-- Wrapper for Slides
-        <div class="carousel-inner">
-					<div class="offertCenter">
-					<h2 class="offerTitle">Biura i Sklepy</h2>
-					</div>
-
-
-            <div class="item active">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 1</h2>
-                </div>
-            </div>
-            <div class="item">
-                <!-- Set the second background image using inline CSS below.
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 2</h2>
-                </div>
-            </div>
-            <div class="item">
-                <!-- Set the third background image using inline CSS below.
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 3</h2>
-                </div>
-            </div>
-        </div>
-
-      
-	</div>
-    </header>
-	
-		<div class="col-md-6 offerSlider">
-			    <!-- Full Page Image Background Carousel Header 
-    <header id="myCarousel2" class="carousel slide">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel2" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel2" data-slide-to="1"></li>
-            <li data-target="#myCarousel2" data-slide-to="2"></li>
-        </ol>
-
-        <!-- Wrapper for Slides -->
-        <div class="carousel-inner">
-        	<h2 class="text-center offerTitle">Biura i Sklepy</h2>
-            <div class="item active">
-                <!-- Set the first background image using inline CSS below. 
-                <div class="fill"><span></span></div>
-                <div class="carousel-caption">
-                    <h2>Caption 1</h2>
-                </div>
-            </div>
-            <div class="item">
-                <!-- Set the second background image using inline CSS below. 
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 2</h2>
-                </div>
-            </div>
-            <div class="item">
-                <!-- Set the third background image using inline CSS below.
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 3</h2>
-                </div>
-            </div>
-        </div>
-
-        
-
-    </header>
-</div>
-</section>
- -->	<footer class="container">
-		<div class="row">
-			<div class="col-md-3">
-				<hgroup>
-					<h3>Dane kontaktowe</h3>
-					<h4>Firma Krzak</h4>
-				</hgroup>
-				<address>
-					<p>ul. Krzak 2</p>
-					<p>66-554 Wawa</p>
-					<p>tel. 4444 444 44</p>
-				</address>
-			</div>
-			<div class="col-md-5">
-				<h3>Mapa</h3>
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9914406081784!2d2.292292615536884!3d48.858373608662156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sWie%C5%BCa+Eiffla!5e0!3m2!1spl!2spl!4v1450644230821" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-			</div>
-			<div class="col-md-4">
-				<h3>Szybki kontakt</h3>
-				<div class="contactForm">
-					<form method="post" action="enrollment.php">
-						<input name="name" placeholder="Twoje imię i nazwisko">
-						<input name="email" type="email" placeholder="Twój e-mail">
-						<input name="tel" type="tel" placeholder="Twój numer telefonu">
-						<textarea name="message" placeholder="Napisz tu wiadomość"></textarea>
-						<input id="submit" name="submit" type="submit" value="Wyślij">
-					</form>
-				</div><!-- contactForm -->
+	<section class="services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5 servicesContent col-md-offset-1">
+					<a href="">
+						<div class="serviceImgContent">
+							<p>Zobacz więcej</p>
+							<img class="center-block text-center servicesImg img-responsive" src="img/serviceImg.jpg" alt="">
+						</div>
+					</a>
+					<h2 class="text-center">Domy i Mieszkania</h2>
+					<p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia nobis nam inventore labore soluta! Labore error, esse optio. Animi, in!</p>
+				</div>
+				<div class="col-md-5 servicesContent">
+					<a href="">
+						<div class="serviceImgContentTwo">
+							<p>Zobacz więcej</p>
+							<img class="center-block text-center servicesImg img-responsive" src="img/serviceImg.jpg" alt="">
+						</div>
+					</a>
+					<h2 class="text-center">Biura i Sklepy</h2>
+					<p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur in nostrum saepe maiores iusto natus beatae, perspiciatis totam architecto sint!</p>
+				</div>
 			</div>
 		</div>
+	</section>
+
+
+	<section class="quality">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 text-center">
+					<div class="qualityContent">
+						<i class="fa fa-3x fa-calendar"></i>
+						<h3>Terminowość</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In animi repellendus cum error esse qui ut possimus assumenda modi fugiat harum reiciendis itaque illum, iste sapiente quidem, exercitationem officia minima?
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4 text-center">
+					<div class="qualityContent">
+						<i class="fa fa-3x fa-diamond"></i>
+						<h3>Jakość</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque doloremque, pariatur rerum error incidunt dignissimos eaque ipsa, officiis quis consequuntur repellat architecto perspiciatis quas commodi commodic ommodi</p>
+				</div>
+				</div>
+				<div class="col-md-4 text-center">
+					<div class="qualityContent">
+						<i class="fa fa-3x fa-money"></i>
+						<h3>Cena</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus repudiandae, impedit cupiditate quasi deserunt illum sequi dolores atque, reprehenderit 	sapiente esse, quo minima voluptatibus non beatae optio nostrum odit. Sequi!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer class="footerAddress">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 footerAddressData">
+					<address>
+						<h4>zrb mradex</h4>
+						<p><i class="fa fa-mobile fa-lg"></i> 692 528 027</p>
+						<p><i class="fa fa-envelope-o"></i> kontakt@nazwastrony.pl</p>
+						<p><i class="fa fa-globe"></i> <a href="www.nazwastrony.pl">www.nazwastrony.pl</a></p>
+						<p>Gostkowo 20A</p>
+						<p>07 - 215, Obryte</p>
+						<p>NIP 762-190-07-26</p>
+
+					</address>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+
+				<div class="col-md-4">
+					
+					<div class="contactForm">
+						<h4>szybki kontakt</h4>
+						<form method="post" action="enrollment.php">
+							<input name="name" placeholder="Twoje imię i nazwisko">
+							<input name="tel" type="tel" placeholder="Twój numer telefonu">
+							<textarea name="message" placeholder="Napisz tu wiadomość"></textarea>
+							<input id="submit" name="submit" type="submit" value="Wyślij">
+							</div>
+						</form>
+					</div><!-- contactForm -->
+				</div>
+			</div><!-- row -->
+		</div><!-- container -->
 	</footer>
 
-	<!-- Script to Activate the Carousel -->
 
 	</body>
 <html>
